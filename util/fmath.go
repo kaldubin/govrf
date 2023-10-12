ac0c7fb97e90af487bc770590303c3ed3fabbbe2
BOITIER William <william.boitier@cea.fr>
2023-10-12 17:28:34 +0200

    Rework fmath for big lib

--- util/fmath.go
+++ util/fmath.go
@@ -1,21 +1,32 @@
 package util
 
-import "math"
+import (
+	"math"
+	"math/big"
+)
 
-func Sgn0Meq1(x int) int {
-	return x % 2
+func Sgn0Meq1(x *big.Int) *big.Int {
+	var sgn *big.Int
+	return sgn.Mod(x, big.NewInt(2))
 }
 
-func IsSquare(x int, q int) bool {
-	var x_pow int = int(math.Pow(float64(x), float64((q-1)/2))) % q
-	if x_pow == 0 || x_pow == 1 {
+func IsSquare(x *big.Int, q *big.Int) bool {
+	var x_pow *big.Int
+	var exp *big.Int
+	exp.Add(q, big.NewInt(-1))
+	exp.Div(exp, big.NewInt(2))
+	x_pow.Exp(x, exp, q)
+	if x_pow.Cmp(big.NewInt(0)) == 0 || x_pow.Cmp(big.NewInt(1)) == 0 {
 		return true
 	}
 	return false
 }
 
-func Inv0(x int, q int) int {
-	return int(math.Pow(float64(x), float64(q-2))) % q
+func Inv0(x *big.Int, q *big.Int) *big.Int {
+	var ret *big.Int
+	var q_minus_2 *big.Int
+	q_minus_2.Add(q, big.NewInt(-2))
+	return ret.Exp(x, q_minus_2, q)
 }
 
 func LegendreSymbol(x int, q int) int {
